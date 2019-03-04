bf14d986311e33500d7c478a306a09fbb691a006
Anika Tahsin <atahsin171107@bscse.uiu.ac.bd>
2019-03-04 22:54:14 +0600

    Create Digit root.cpp

---
+++ Educational codeforces round 59/Digit root.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main(){
+   ll q, n, f, k, a;
+   cin >> q;
+   while(q -- ){
+      cin >> n >> k;
+      f = k + (9*(n-1));
+      cout << f << endl;
+   }
+    return 0;
+}
+
+
+
